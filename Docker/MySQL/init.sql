45d18576a62144292755927e971e2af0fac5ba75
abyMaya <abycat22@gmail.com>
2024-09-12 18:28:33 +0900

    [update] 既存のデータベースを一旦削除

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -1,5 +1,11 @@
+DROP DATABASE SUN_Chaser;
+DROP USER 'admin';
+
+CREATE USER 'admin' IDENTIFIED BY 'chaser';
 CREATE DATABASE SUN_Chaser;
 USE SUN_Chaser;
+GRANT ALL PRIVILEGES ON gutara_chat.* TO 'admin';
+
 CREATE TABLE Users (
     id INT AUTO_INCREMENT PRIMARY KEY
 );
