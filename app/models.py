136ba1d3c3c3029bffb056e1b59c14fa08cfa477
abyMaya <abycat22@gmail.com>
2024-09-13 23:59:53 +0900

    [update] signup処理動作確認済み

--- app/models.py
+++ app/models.py
@@ -1,33 +1,46 @@
-from flask import abort
-import pymysql
+from flask import abort, app
+import pymysql # type: ignore
 from util.DB import DB
-
+from app import app
 
 class dbConnect:
+    
     def createUser(user_id, user_name, email, password, created_at):
+        conn = None
+        cur = None
         try:
             conn = DB.getConnection()
             cur = conn.cursor()
-            sql = "INSERT INTO users (uid, user_name, email, password) VALUES (%s, %s, %s, %s, %s);"
+            sql = "INSERT INTO Users (user_id, user_name, email, password, created_at) VALUES (%s, %s, %s, %s, %s);"
             cur.execute(sql, (user_id, user_name, email, password, created_at))
             conn.commit()
+            
         except Exception as e:
-            print(str(e) + 'が発生しています')
+            print(e + 'が発生しています')
             abort(500)
         finally:
-            cur.close()
+            if cur:
+                cur.close()  
+            if conn:
+                conn.close()
 
 
     def getUser(email):
+        conn = None
+        cur = None
         try:
             conn = DB.getConnection()
             cur = conn.cursor()
-            sql = "SELECT * FROM users WHERE email=%s;"
-            cur.execute(sql, (email))
+            sql = "SELECT * FROM Users WHERE email=%s;"
+            cur.execute(sql, (email,))
             user = cur.fetchone()
             return user
         except Exception as e:
-            print(str(e) + 'が発生しています')
+            print(e + 'が発生しています')
             abort(500)
         finally:
-            cur.close()
+            if cur:
+                cur.close()  
+            if conn:
+                conn.close()
+       
