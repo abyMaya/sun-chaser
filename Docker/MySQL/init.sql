c95da7dcae8599fb3f78fb5cc08c9b49b65c46ef
abyMaya <abycat22@gmail.com>
2024-09-14 00:00:02 +0900

    [update] signup処理動作確認済み

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -11,5 +11,6 @@ CREATE TABLE Users (
     user_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
