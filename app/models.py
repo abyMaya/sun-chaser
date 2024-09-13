b2917053aea66a1f9558d2867f7bc75e1faab1c4
abyMaya <abycat22@gmail.com>
2024-09-13 15:08:45 +0900

    [fix]

--- app/models.py
+++ app/models.py
@@ -2,18 +2,32 @@ from flask import abort
 import pymysql
 from util.DB import DB
 
+
 class dbConnect:
     def createUser(user_id, user_name, email, password, created_at):
         try:
             conn = DB.getConnection()
             cur = conn.cursor()
-            sql = "INSERT INTO users (user_id, user_name, email, password, created_at) VALUES (%s, %s, %s, %s, %s)";
+            sql = "INSERT INTO users (uid, user_name, email, password) VALUES (%s, %s, %s, %s, %s);"
             cur.execute(sql, (user_id, user_name, email, password, created_at))
             conn.commit()
         except Exception as e:
-            print(e + 'が発生しています')
+            print(str(e) + 'が発生しています')
             abort(500)
         finally:
             cur.close()
 
-    
+
+    def getUser(email):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM users WHERE email=%s;"
+            cur.execute(sql, (email))
+            user = cur.fetchone()
+            return user
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
