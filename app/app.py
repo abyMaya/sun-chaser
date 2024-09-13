54e58c5dd49ab76d5436e258bd11224ca471a4f4
abyMaya <abycat22@gmail.com>
2024-09-13 12:31:06 +0900

    [update] signup処理記述

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import uuid
+from flask import Flask, render_template, request, flash, redirect, session
+import uuid, re, hashlib
 from datetime import timedelta
 
 from models import dbConnect
@@ -18,7 +18,43 @@ def sun_chaser():
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
-# 新規登録機能
+
+# 新規登録処理
+@app.route('/signup', method=['POST'])
+def userSignup():
+    user_name = request.form.get('user_name')
+    email = request.form.get('email')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+
+    pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+    if user_name == '' or email == '' or password1 == '' or password1 == '':
+        flash('全てのフォームに入力してください')
+    elif password1 != password2:
+        flash('２つのパスワードの値が違っています')
+    elif re.match(pattern, email) is None:
+        flash('正しいメールアドレスの形式ではありません')
+    else:
+        user_id = uuid.uuid4()
+        password = hashlib.sha256(password1.encode('utf-8')).hexdigest()
+        DBuser = dbConnect.getUser(email)
+
+        if DBuser != None:
+            flash('既に登録されています')
+        else:
+            dbConnect.createUser(user_id, username, email, password)
+            UserId = str(user_id)
+            session['user_id'] = UserId
+            return redirect('/')
+    return redirect('/signup')
+
+
+
+
+
+    
+
 
 # ログインページ表示
 @app.route('/')
