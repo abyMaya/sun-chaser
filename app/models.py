6d63e6c8fdfe6d506d2a38d0455f655b748564de
abyMaya <abycat22@gmail.com>
2024-09-23 17:08:01 +0900

    [update]ユーザー名変更　動作確認済み

--- app/models.py
+++ app/models.py
@@ -57,3 +57,24 @@ class dbConnect:
             return None
         finally:
             connection.close()
+
+    def updateUser(user_id, new_username):
+        conn = None
+        cur = None
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "UPDATE Users SET user_name = %s WHERE user_id = %s;"
+            cur.execute(sql, (new_username, user_id))
+            conn.commit()
+        except Exception as e:
+            print(e + 'が発生しています')
+            abort(500)
+        finally:
+            if cur:
+                cur.close()
+            if conn:
+                conn.close()
+
+
+            
