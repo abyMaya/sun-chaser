e2d8af6112d9990451bd9d36de65dbdac76bd0b1
abyMaya <abycat22@gmail.com>
2025-03-08 20:31:03 +0900

    [chore]reset eslint

---
+++ lint/eslintrc.cjs
@@ -0,0 +1,15 @@
+const prettier = require("eslint-plugin-prettier");
+
+module.exports = [
+  {
+    rules: {},
+  },
+  {
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      "prettier/prettier": ["error"],
+    },
+  },
+];
